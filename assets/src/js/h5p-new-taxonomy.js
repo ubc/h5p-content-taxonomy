54b915b4ae3cecd6ff293f3d168c8bf9e319e7ca
kelvin <kelvin.xu@ubc.ca>
2022-04-29 08:40:24 -0700

    UI components improvements.
    1. Remove Faculty and Discipline components and made a generic Select components.
    2. Added JavaScript filters in multiple places.
    3. Add documentations

--- assets/src/js/h5p-new-taxonomy.js
+++ assets/src/js/h5p-new-taxonomy.js
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import 'url-search-params-polyfill';
-import Faculty from './h5p-new-taxonomy-faculty';
-import Discipline from './h5p-new-taxonomy-discipline';
 import MetaBoxWrap from './h5p-new-taxonomy-metabox-wrap';
+import { format2levelTermsOptions, retriveObjectsFrom2levelTermsOptions } from './helper.js';
+import Select from './select2';
 
 export default ( props ) => {
     
@@ -10,35 +10,55 @@ export default ( props ) => {
     const [disciplineSelected, setDisciplineSelected] = useState([]);
 
     useEffect(() => {
-        const selectedFacultyFromDB = ubc_h5p_admin.content_faculty ? ubc_h5p_admin.content_faculty : [];
-        setFacultySelected( selectedFacultyFromDB && selectedFacultyFromDB.length !== 0 ? selectedFacultyFromDB : ( ubc_h5p_admin.user_faculty ? ubc_h5p_admin.user_faculty.map( faculty_id => {
-            return parseInt( faculty_id );
-        }) : [] ) );
+        /**
+         * Few conditions to check with priority
+         * 
+         * 1. If the content already has faculties attached, then we use the faculties saved from database.
+         * 2. If the content does not have faculty attached, then we set the user's faculties as default.
+         */
+        const selectedFacultyFromDB = ubc_h5p_admin.content_faculty ? retriveObjectsFrom2levelTermsOptions(ubc_h5p_admin.content_faculty, ubc_h5p_admin.faculties_list) : [];
+        const userFaculty = ubc_h5p_admin.user_faculty ? retriveObjectsFrom2levelTermsOptions(ubc_h5p_admin.user_faculty, ubc_h5p_admin.faculties_list) : [];
+        setFacultySelected( selectedFacultyFromDB && selectedFacultyFromDB.length !== 0 ? selectedFacultyFromDB : ( userFaculty ? userFaculty : [] ) );
 
-        const selectedDisciplineFromDB = ubc_h5p_admin.content_discipline ? ubc_h5p_admin.content_discipline : [];
+        /**
+         * If the content already has disciplines attached, then we use the disciplines save from database.
+         */
+        const selectedDisciplineFromDB = ubc_h5p_admin.content_discipline ? retriveObjectsFrom2levelTermsOptions(ubc_h5p_admin.content_discipline, ubc_h5p_admin.disciplines_list) : [];
         setDisciplineSelected( selectedDisciplineFromDB ? selectedDisciplineFromDB : [] );
     }, []);
 
     return (
         <div>
             <MetaBoxWrap label="Faculty">
-                <Faculty
-                    facultySelected={ facultySelected }
-                    setFacultySelected={ setFacultySelected }
+                 <Select
+                    selected={ facultySelected }
+                    isMulti={ true }
+                    options={ format2levelTermsOptions(ubc_h5p_admin.faculties_list) }
+                    placeholder="Select Faculty..."
+                    setSelected={ setFacultySelected }
+                    name="ubc-h5p-content-faculty"
                 />
             </MetaBoxWrap>
 
             <MetaBoxWrap label="Discipline">
-                <Discipline
-                    disciplineSelected={ disciplineSelected }
-                    setDisciplineSelected={ setDisciplineSelected }
+                <Select
+                    selected={ disciplineSelected }
+                    isMulti={ true }
+                    options={ format2levelTermsOptions(ubc_h5p_admin.disciplines_list) }
+                    placeholder="Select Discipline..."
+                    setSelected={ setDisciplineSelected }
+                    name="ubc-h5p-content-discipline"
                 />
             </MetaBoxWrap>
             <input
                 type="hidden"
                 value={ JSON.stringify({
-                    faculty: facultySelected,
-                    discipline: disciplineSelected
+                    faculty: facultySelected.map(faculty => {
+                        return faculty.value;
+                    }),
+                    discipline: disciplineSelected.map(discipline => {
+                        return discipline.value;
+                    })
                 }) }
                 name="ubc-h5p-content-taxonomy"
             />
